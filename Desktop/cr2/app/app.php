454363157fbcd4e97d6282889d0d4efa5c137e3b
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-27 20:44:59 -0700

    add folder structure, add app.php add index.php, everything set up to begin running tests

---
+++ Desktop/cr2/app/app.php
@@ -0,0 +1,22 @@
+<?php
+
+    require_once __DIR__.'/../vendor/autoload.php';
+    // class(s) goes here
+
+    //required for silex
+    $app = new Silex\Application();
+
+    // allows twig to be used in the views folders
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    // points to home page after tests pass
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('form.html.twig');
+    });
+
+
+    // always needed to actually return the app 
+    return $app;
+ ?>
