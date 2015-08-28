a5cfb70ff36dd5bf5f012cb2755f1f00163ff5d1
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-27 20:50:32 -0700

    create class RepeatCounter, create test file install composter and phpunit tests

---
+++ Desktop/cr2/src/ReapeatCounter.php
@@ -0,0 +1,13 @@
+<?php
+    // class name
+    class RepeatCounter
+    // where properties would go if needed
+
+        // begin function construct and establish variables?
+    {
+        function countRepeats ($word, $string)
+        {
+
+
+
+?>
---
+++ Desktop/cr2/tests/RepeatCounterTest.php
@@ -0,0 +1,11 @@
+<?php
+    // links back to the class in the src folder
+    require_once "src/RepeatCounter.php";
+
+    // actually needed so you can run phpunit tests 
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    {
+
+
+
+?>
