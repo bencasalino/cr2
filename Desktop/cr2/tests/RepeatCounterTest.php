041ff23187146e81d069c73b870f7d9be964d4c2
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-27 20:57:13 -0700

    did this commit work?

--- Desktop/cr2/tests/RepeatCounterTest.php
+++ Desktop/cr2/tests/RepeatCounterTest.php
@@ -2,9 +2,24 @@
     // links back to the class in the src folder
     require_once "src/RepeatCounter.php";
 
-    // actually needed so you can run phpunit tests 
+    // actually needed so you can run phpunit tests
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
+      //Test 1
+      function test_countRepeats_sameWordCount()
+      {
+        //Arrange
+        $word = "";
+        $string = "":
+        //Act
+
+        //Assert
+        $this->assertEquals();
+      }
+
+
+
+    }
 
 
 
