4ec2c7df3c63ff1ca4966123de9da646b9500b90
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-08-27 21:02:42 -0700

    2nd test pass (words are not the same, and words are the same pass )

--- Desktop/cr2/tests/RepeatCounterTest.php
+++ Desktop/cr2/tests/RepeatCounterTest.php
@@ -19,12 +19,25 @@
 
         //Assert
         $this->assertEquals(1, $result);
-      
+
       }
 
+      //Test 2 are the words not the same???
+      function test_countRepeats_differentWord()
+      {
+          //Arrange
+          $test_RepeatCounter = new RepeatCounter;
+          $word = "piano";
+          $string = "pianos";
 
+          //Act
+          $test_RepeatCounter->countRepeats($word, $string);
+          $result =
 
-    }
+          //Assert
+          $this->assertEquals(" Output word isnt the same as the input word", $result);
+
+      }
 
 
 
